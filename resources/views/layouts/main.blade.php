04d7df3887b96561ac69f163fa5589e519cd8414
sulisrosliani04 <203040011@mail.unpas.ac.id>
2022-11-03 22:17:06 -0700

    menyelesaikan video 14

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,13 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
+    <!-- {{-- Bootstrap Icons --}} -->
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <!-- {{-- My Style --}} -->
+    <link rel="stylesheet" href="/css/style.css">
+
     <title>Sulis Rosliani blog | {{ $title }}</title>
 </head>
 
